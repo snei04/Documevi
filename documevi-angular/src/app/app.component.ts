b7e3487be6155a9a0b79816c368ba7d2fbed3843
Sneider Fuquen <sneifubernal@gmail.com>
2024-11-19 09:27:57 -0500

    trabajo en angular

--- documevi-angular/src/app/app.component.ts
+++ documevi-angular/src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule,RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
+ 
 })
 export class AppComponent {
-  title = 'my-app';
+  welcome = 'Hola!!';
+  tasks = [
+    'instalar angular',
+    'ejecutar la aplicacion',
+    'instalar componentes'
+  ]
 }

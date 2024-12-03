14dc934b664066bbf91dc6d44ea18af06b67e9a2
Sneider Fuquen <sneifubernal@gmail.com>
2024-12-03 17:21:38 -0500

    actualizacion y unificacion de manu y home

--- js/home.js
+++ js/home.js
@@ -12,3 +12,15 @@ menuBtn.addEventListener('click', () => {
     lineThree.classList.toggle('line-cross');
     link.classList.toggle('fade-in');
 })
+
+
+// Seleccionamos todos los elementos con la clase 'Dropdown'
+const dropdowns = document.querySelectorAll('.Dropdown');
+
+// Añadimos un evento de click a cada uno de los elementos seleccionados
+dropdowns.forEach(function(dropdown) {
+  dropdown.addEventListener('click', function() {
+    // Alternamos la clase 'is-expanded' en el elemento que fue clickeado
+    this.classList.toggle('is-expanded');
+  });
+});

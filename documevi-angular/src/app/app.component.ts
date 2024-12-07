039aea0358cb0a301aaa1cb6caaebd00cb5e53d7
Sneider Fuquen <sneifubernal@gmail.com>
2024-12-07 13:17:15 -0500

    union de home

--- documevi-angular/src/app/app.component.ts
+++ documevi-angular/src/app/app.component.ts
@@ -1,20 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule,RouterOutlet],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
- 
 })
 export class AppComponent {
-  welcome = 'Hola!!';
-  tasks = [
-    'instalar angular',
-    'ejecutar la aplicacion',
-    'instalar componentes'
-  ]
+  title = 'documevi-angular';
 }

400ca8edf2e2556db7317a437ddcc5a84997b0ea
Sneider Fuquen <sneifubernal@gmail.com>
2025-02-20 09:08:12 -0500

    Creacion olvido-contraseña

--- documevi-angular/frontend/documevi/src/app/app.routes.ts
+++ documevi-angular/frontend/documevi/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { InicioComponent } from './pages/inicio/inicio.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { OlvidoContrasenaComponent } from './pages/olvido-contrasena/olvido-contrasena.component';
 export const routes: Routes = [
 
     {
@@ -11,8 +12,11 @@ export const routes: Routes = [
         path: 'dashboard',
         component: DashboardComponent
     
+    },
+    {
+        path: 'olvidarContraseña',
+        component: OlvidoContrasenaComponent
     }
-    
 
 
 
--- documevi-angular/frontend/documevi/src/app/pages/olvido-contrasena/olvido-contrasena.component.ts
+++ documevi-angular/frontend/documevi/src/app/pages/olvido-contrasena/olvido-contrasena.component.ts
@@ -1,11 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-olvido-contrasena',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './olvido-contrasena.component.html',
-  styleUrl: './olvido-contrasena.component.css'
+  styleUrls: ['./olvido-contrasena.component.css']
 })
-export class OlvidoContrasenaComponent {
+export class OlvidoContrasenaComponent  {
+
+  form: HTMLElement | null | undefined;
+  password: HTMLInputElement | null | undefined;
+  confirmPassword: HTMLInputElement | null | undefined;
+  errorMessage: HTMLElement | null | undefined;
+  successMessage: HTMLElement | null | undefined;
+
+  ngOnInit() {
+    this.form = document.getElementById('passwordForm');
+    this.password = document.getElementById('password') as HTMLInputElement;
+    this.confirmPassword = document.getElementById('confirmPassword') as HTMLInputElement;
+    this.errorMessage = document.getElementById('errorMessage');
+    this.successMessage = document.getElementById('successMessage');
+
+    this.form?.addEventListener('submit', (event): void => {
+      event.preventDefault(); // Evitar el envío del formulario
+
+      if (this.password?.value === this.confirmPassword?.value) {
+      if (this.errorMessage) this.errorMessage.style.display = 'none';
+      if (this.successMessage) this.successMessage.style.display = 'block';
+      } else {
+      if (this.errorMessage) this.errorMessage.style.display = 'block';
+      if (this.successMessage) this.successMessage.style.display = 'none';
+      }
+    });
+  }
+ 
+  onSubmit(passwordForm: NgForm): void {
+
+    console.log(passwordForm.value);
+
+  }
 
 }
